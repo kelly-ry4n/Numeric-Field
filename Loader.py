5e56c42096050fbc0041968764b9290fc6f75037
Kelly Ryan <kelly.ry4n@gmail.com>
2012-12-17 23:48:16 -0800

    Loader created

--- Loader.py
+++ Loader.py
@@ -1,1 +1,12 @@
-Loader.py
+
+def get_data(filename):
+    with open(filename, 'r') as f:
+        data = f.read()
+
+    data = data.split('\n')
+    data = (i.split(',') for i in data)
+    data = zip(*data)
+    return data[0],data[1],data[2]
+
+if __name__ == '__main__':
+    print get_data('./Saves/t1.txt')
