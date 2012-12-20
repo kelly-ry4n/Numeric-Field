2c293fed2f51110844fa9c8deb3ff3d51a8fbdec
Kelly Ryan <kelly.ry4n@gmail.com>
2012-12-19 19:57:11 -0800

    Fixed save bug

--- Files.py
+++ Files.py
@@ -6,13 +6,14 @@ def get_data(filename):
     data = data.split('\n')
     data = (i.split(',') for i in data)
     data = zip(*data)
-    
+
     return data[0],data[1],data[2]
 
 def save_data(filename,xs,ys,cs):
     with open(filename, 'w') as f:
-        for x,y,c in zip(xs,ys,cs):
+        for x,y,c in zip(xs[:-1],ys[:-1],cs[:-1]):
             f.write('%s,%s,%s\n' % (x,y,c))
+        f.write('%s,%s,%s' % (x,y,c))
 
 if __name__ == '__main__':
-    print get_data('./Saves/test.txt')
+    print get_data('./Saves/test.figsave')
